0b9356d88416784467f2c57cc452c84a7ddd64c9
Nishtha Srivastava <51073931+srivastavanishtha@users.noreply.github.com>
2021-06-24 16:28:26 +0530

    Update GGPLOT2.r

--- GGPLOT2.r
+++ GGPLOT2.r
@@ -5,3 +5,9 @@ mtcars$am <- factor(mtcars$am,levels=c(0,1),
 mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
                       labels=c("4cyl","6cyl","8cyl")) 
   qplot(mpg, data=mtcars, geom="density", fill=gear, alpha=I(.5),main="Distribution of Gas Mileage", xlab="Miles Per Gallon",ylab="Density")
+qplot(hp,mpg, data=mtcars, geom="line", alpha=I(.5), 
+       main="miles per gallon vs horse power", xlab="Horse Power",ylab="Miles Per Gallon",colour = I("red"))
+#Scatterplot of mpg vs. hp for each combination of gears and cylinders
+ # in each facet, transmittion type is represented by shape and color
+ qplot(hp, mpg, data=mtcars, shape=am, color=am,        facets=gear~cyl, size=I(3), xlab="Horsepower", ylab="Miles per Gallon") 
+
