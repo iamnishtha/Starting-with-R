7f35bfbfe3a198d51bc79b16c5d4eafb2e76fcf6
Nishtha Srivastava <51073931+srivastavanishtha@users.noreply.github.com>
2021-06-24 16:23:21 +0530

    Create sumofnaturalnumbers.r

---
+++ sumofnaturalnumbers.r
@@ -0,0 +1,24 @@
+#without formula
+# take input from the user
+num = as.integer(readline(prompt = "Enter a number: "))
+if(num < 0) {
+print("Enter a positive number")
+} else {
+sum = 0
+# use while loop to iterate until zero
+while(num > 0) {
+sum = sum + num
+num = num - 1
+}
+print(paste("The sum is", sum))
+}
+
+#wih formula
+# take input from the user
+num = as.integer(readline(prompt = "Enter a number: "))
+if(num < 0) {
+print("Enter a positive number")
+} else {
+sum = (num * (num + 1)) / 2;
+print(paste("The sum is", sum))
+}
